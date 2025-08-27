35fcdf911a2af05cbddd8676febd69af1d67c229
snowflakescoding <ducnghiepdao@gmail.com>
2025-08-27 22:19:58 +0700

    add lab 01 (or week 01)

---
+++ Lab_01/Exercise_01.py
@@ -0,0 +1,8 @@
+# printing the first line with input() function
+name = str(input("What is your name? "))
+
+# printing the second line
+print(f"Welcome to 62FIT3SS1 class, {name}!")
+
+# printing the final line
+print("May the force be with you.")
---
+++ Lab_01/Exercise_02.py
@@ -0,0 +1,8 @@
+# print the celsius line
+celsius = int(input("Enter temperature in Celsius: "))
+
+# formula for the fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+# print the fahrenheit line
+print(f"The temperature in Fahrenheit is: {fahrenheit}")
