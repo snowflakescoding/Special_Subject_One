4ec337a4e327d189b173d807b88cf3fcf67d2309
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-10 18:28:24 +0700

    Create Example_02.py

---
+++ Lab_03/Example_02.py
@@ -0,0 +1,20 @@
+#task:  python function that asks user to input an integer which must be an odd number from 1 to 50.
+
+def inputValidation():
+    while True:
+        try:
+            number = int(input("Enter a odd integer from 1 to 50: ")) # input a number
+            
+            if number < 1 or number > 50: # case 1: out of the range between 1 and 50
+                print("The number must be an odd number from 1 to 50")
+            
+            elif number % 2 == 0: # case 2: if case 1 passes, case 2 will check if it is an odd or even number. if even = print the same line as the first case
+                print("The number must be an odd number from 1 to 50")
+            
+            else: # case 3: if case 1 and 2 are all passed
+                return number
+                
+        except ValueError:
+            print("Please enter an integer")
+
+# process: while loop -> try/except -> follow the question
