25977d021d670332e6f63613ed837ad8de6a9fec
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-12 18:50:57 +0700

    Update Exercise_01_2nd_way.py

--- Lab_03/Exercise_01/Exercise_01_2nd_way.py
+++ Lab_03/Exercise_01/Exercise_01_2nd_way.py
@@ -1,1 +1,12 @@
+# task: build a python function to check whether a number is a prime number or not
 
+# 2nd way: not importing math
+
+def isPrime(n): 
+    if n < 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0: 
+                return False
+        return True
